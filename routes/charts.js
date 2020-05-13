98c65b378a6ef18a7f14c808de3cbdcb80057f44
Markus Wiland <markus.wiland@outlook.com>
2020-05-13 10:33:33 +0200

    ändrade till orderby och limit

--- routes/charts.js
+++ routes/charts.js
@@ -24,20 +24,12 @@ router.get('/bottom', async (req, res) => {
 
     let array = []
 
-    let snapshot = await db.collection('hamsters').get()
+    let snapshot = await db.collection('hamsters').orderBy('defeats', 'desc').limit(5).get()
 
     snapshot.forEach(doc => {
         array.push(doc.data())
     })
 
-    // sorterar från lägst till högst
-    array.sort((a, b) => {
-        return b.id - a.id
-    })
-
-    // kör en kopia av arrayen och slicear den och tar ut 5 första.
-    let topFive = array.slice(0, 5)
-    console.log(topFive)
     res.status(200).send(array)
 
 })
