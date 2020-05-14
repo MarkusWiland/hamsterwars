993dfbfdc746cba43d84c4df0aa50ca2f5cb21e8
Markus Wiland <markus.wiland@outlook.com>
2020-05-14 09:04:38 +0200

    adding some comments

--- routes/charts.js
+++ routes/charts.js
@@ -5,15 +5,16 @@ const router = new Router();
 
 router.get('/top', async (req, res) => {
     try {
-        let array = []
-
+        // create empty array
+        let chartsArray = []
+        // get into collection hamster and go trough wins and get 5 with most wins.
         let snapshot = await db.collection('hamsters').orderBy('wins', 'desc').limit(5).get()
 
         snapshot.forEach(hamster => {
-            array.push(hamster.data())
+            chartsArray.push(hamster.data())
         })
 
-        res.status(200).send(array)
+        res.status(200).send(chartsArray)
     } catch (err) {
         console.error(err)
         res.status(500).send(err)
@@ -25,15 +26,16 @@ router.get('/top', async (req, res) => {
 
 router.get('/bottom', async (req, res) => {
     try {
-        let array = []
-
+        let chartsArray = []
+        // get into collection hamster and go trough defeats and get 5 with most defeats.
         let snapshot = await db.collection('hamsters').orderBy('defeats', 'desc').limit(5).get()
 
         snapshot.forEach(doc => {
-            array.push(doc.data())
+            // push the data into the empty array
+            chartsArray.push(doc.data())
         })
 
-        res.status(200).send(array)
+        res.status(200).send(chartsArray)
     } catch (err) {
         console.error(err)
         res.status(500).send(err)
